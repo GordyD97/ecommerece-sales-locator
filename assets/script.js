f0c04504902c3b4b3c14a9218d1df632b4b63ba4
Steffan Carter <xisled09@gmail.com>
2022-04-13 20:56:26 -0400

    fetching items working

--- assets/script.js
+++ assets/script.js
@@ -1,16 +1,25 @@
-var repoList = document.querySelector('#testEl');
-var fetchButton = document.getElementById('fetch-button');
+var repoList = document.querySelector('#listEl');
+var fetchButton = document.querySelector('.fetch-button');
 
 function getApi() {
-  var requestUrl = 'https://upenn-cors-anywhere.herokuapp.com/api.itbook.store%2F1.0%2Fsearch%2Fmongodb';
+    var requestUrl = 'https://kitsu.io/api/edge/anime?filter[categories]=adventure';
     // encodeURIcomponent
-    fetch('https://api.itbook.store/1.0/search/mongodb')
+    fetch('https://kitsu.io/api/edge/anime?filter[categories]=adventure')
     .then(function (response) {
       return response.json();
     })
     .then(function (data) {
-      console.log(data)
+        console.log(data.data.length)
+        repoList.innerHTML = ''
+        for (var i = 0; i < data.data.length; i++) {
+            console.log('test')
+            var listItem = document.createElement('li');
+            listItem.innerHTML = data.data[i].attributes.canonicalTitle;
+        
+            repoList.appendChild(listItem);
+          }
+        console.log(data)
     });
 }
 
-window.addEventListener('click', getApi);
+fetchButton.addEventListener('click', getApi);
