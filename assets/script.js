f1d01205f57fbf889f7c71e185909dd7193e0767
Steffan Carter <xisled09@gmail.com>
2022-04-18 21:26:58 -0400

    overflow fix

--- assets/script.js
+++ assets/script.js
@@ -36,35 +36,35 @@ document.getElementById("newManga")
 });
 
 
-// var requestOptions = {
-//   method: 'GET',
-// };
+var requestOptions = {
+  method: 'GET',
+};
 
-// fetch("https://api.geoapify.com/v2/places?categories=commercial.books&filter=circle:-75.371964,39.887065,9000&bias=proximity:-75.371964,39.887065&limit=20&apiKey=3f037f71cb1e42579fb3ff5aa319aeee", requestOptions)
-//   .then(response => response.json())
-//   .then(result => console.log(result))
-//   .catch(error => console.log('error', error));
+fetch("https://api.geoapify.com/v2/places?categories=commercial.books&filter=circle:-75.371964,39.887065,9000&bias=proximity:-75.371964,39.887065&limit=20&apiKey=3f037f71cb1e42579fb3ff5aa319aeee", requestOptions)
+  .then(response => response.json())
+  .then(result => console.log(result))
+  .catch(error => console.log('error', error));
 
-//   var map = L.map("my-map").setView([39.8694, -75.3824], 10);
+  var map = L.map("my-map").setView([39.8694, -75.3824], 10);
   
-//   // Get your own API Key on https://myprojects.geoapify.com
-//   var myAPIKey = "3f037f71cb1e42579fb3ff5aa319aeee";
+  // Get your own API Key on https://myprojects.geoapify.com
+  var myAPIKey = "3f037f71cb1e42579fb3ff5aa319aeee";
 
-//   // Retina displays require different mat tiles quality
-//   var isRetina = L.Browser.retina;
+  // Retina displays require different mat tiles quality
+  var isRetina = L.Browser.retina;
 
-//   var baseUrl =
-//     "https://maps.geoapify.com/v1/tile/osm-bright/{z}/{x}/{y}.png?apiKey=3f037f71cb1e42579fb3ff5aa319aeee";
-//   var retinaUrl =
-//     "https://maps.geoapify.com/v1/tile/osm-bright/{z}/{x}/{y}@2x.png?apiKey=3f037f71cb1e42579fb3ff5aa319aeee";
+  var baseUrl =
+    "https://maps.geoapify.com/v1/tile/osm-bright/{z}/{x}/{y}.png?apiKey=3f037f71cb1e42579fb3ff5aa319aeee";
+  var retinaUrl =
+    "https://maps.geoapify.com/v1/tile/osm-bright/{z}/{x}/{y}@2x.png?apiKey=3f037f71cb1e42579fb3ff5aa319aeee";
 
-//   // Add map tiles layer. Set 20 as the maximal zoom and provide map data attribution.
-//   L.tileLayer(isRetina ? retinaUrl : baseUrl, {
-//     attribution:
-//       'Powered by <a href="https://www.geoapify.com/" target="_blank">Geoapify</a> | © OpenStreetMap <a href="https://www.openstreetmap.org/copyright" target="_blank">contributors</a>',
-//     apiKey: myAPIKey,
-//     maxZoom: 40,
-//     id: "osm-bright",
-//   }).addTo(map);
+  // Add map tiles layer. Set 20 as the maximal zoom and provide map data attribution.
+  L.tileLayer(isRetina ? retinaUrl : baseUrl, {
+    attribution:
+      'Powered by <a href="https://www.geoapify.com/" target="_blank">Geoapify</a> | © OpenStreetMap <a href="https://www.openstreetmap.org/copyright" target="_blank">contributors</a>',
+    apiKey: myAPIKey,
+    maxZoom: 40,
+    id: "osm-bright",
+  }).addTo(map);
 
   
