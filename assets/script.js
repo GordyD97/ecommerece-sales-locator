249274aab3076fe9a014e19aad69966df19dfbeb
Steffan Carter <xisled09@gmail.com>
2022-04-18 20:41:11 -0400

    styling and text alignment

--- assets/script.js
+++ assets/script.js
@@ -13,13 +13,24 @@ function getApi() {
         for (var i = 0; i < 5; i++) {
             var listItem = document.createElement('li');
             var listImg = document.createElement('img');
-            listItem.innerHTML = data.data[i].title;
+            var listText = document.createElement('p');
             listImg.setAttribute('src', data.data[i].images.webp.image_url); 
             listImg.setAttribute('alt', "title");
-            repoList.appendChild(listItem);
+            
             listItem.appendChild(listImg);
+            listText.textContent = data.data[i].title;
+            listItem.appendChild(listText);
+            repoList.appendChild(listItem);
           }
         console.log(data)
     });
 }
 fetchButton.addEventListener('click', getApi);
+
+document.getElementById("newManga")
+    .addEventListener("keyup", function(event) {
+    event.preventDefault();
+    if (event.keyCode === 13) {
+        document.getElementById("btnFind").click();
+    }
+});
